2ad85dab08cece9bb2597627019e661905629eec
Sz. N. Bálint <szalaynb93@gmail.com>
2018-04-09 12:21:21 +0200

    Further info

---
+++ src/main/java/com/szalaynb/NudgeYourMind/NudgeYourMindApplication.java
@@ -0,0 +1,12 @@
+package com.szalaynb.NudgeYourMind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudgeYourMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NudgeYourMindApplication.class, args);
+	}
+}
